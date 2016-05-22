74d1943c4788d128875df61598ee841c77d9ad25
Florian Adamsky <fa-git@haktar.org>
2016-05-22 19:49:38 +0200

    style: preparation for publishing on hex

--- mix.exs
+++ mix.exs
@@ -2,8 +2,10 @@ defmodule MldhtUmbrella.Mixfile do
   use Mix.Project
 
   def project do
-    [apps_path: "apps",
-     build_embedded: Mix.env == :prod,
+    [app: :mldht,
+     version: "0.1.0",
+     apps_path: "apps",
+     build_embedded:  Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
@@ -22,5 +24,19 @@ defmodule MldhtUmbrella.Mixfile do
   # and cannot be accessed from applications inside the apps folder
   defp deps do
     []
+  end
+
+  defp description do
+    """
+    Distributed Hash Table (DHT) is a storage and lookup system based on a peer-to-peer (P2P) system. The file sharing protocol BitTorrent makes use of a DHT to find new peers without using a central tracker. There are three popular DHT-based protocols: KAD, Vuze DHT, and Mainline DHT. MLDHT, in particular, is an elixir package that provides a mainline DHT implementation according to BEP 05.
+    """
+  end
+
+  defp package do
+    [name:        :mldht,
+     files:       ["apps", "priv", "mix.exs", "README*", "LICENSE*", "license*"],
+     maintainers: ["Florian Adamsky"],
+     licenses:    ["MIT"],
+     links:       %{"GitHub" => "https://github.com/cit/MLDHT"}]
   end
 end
