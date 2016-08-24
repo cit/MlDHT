d48edf74bedf2d7ec1f94b324150710225816121
Florian Adamsky <fa-git@haktar.org>
2016-08-24 21:15:44 +0200

    Removed default comments

--- mix.exs
+++ mix.exs
@@ -7,21 +7,11 @@ defmodule MldhtUmbrella.Mixfile do
      apps_path: "apps",
      build_embedded:  Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options.
-  #
-  # Dependencies listed here are available only for this project
-  # and cannot be accessed from applications inside the apps folder
   defp deps do
     []
   end
@@ -34,6 +24,7 @@ defmodule MldhtUmbrella.Mixfile do
 
   defp package do
     [name:        :mldht,
+     files:       ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
      files:       ["apps", "priv", "mix.exs", "README*", "LICENSE*", "license*"],
      maintainers: ["Florian Adamsky"],
      licenses:    ["MIT"],
