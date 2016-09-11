601ac064af2a645d2c23e98d817425dce58427d6
Florian Adamsky <fa-git@haktar.org>
2016-09-11 13:52:00 +0200

    fix: shorten description

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MlDHT.Mixfile do
 
   def project do
     [app: :mldht,
-     version: "0.1.0",
+     version: "0.0.1",
      elixir: "~> 1.2",
      build_embedded:  Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -32,14 +32,13 @@ defmodule MlDHT.Mixfile do
 
   defp description do
     """
-    Distributed Hash Table (DHT) is a storage and lookup system based on a peer-to-peer (P2P) system. The file sharing protocol BitTorrent makes use of a DHT to find new peers without using a central tracker. There are three popular DHT-based protocols: KAD, Vuze DHT, and Mainline DHT. MLDHT, in particular, is an elixir package that provides a mainline DHT implementation according to BEP 05.
+    Distributed Hash Table (DHT) is a storage and lookup system based on a peer-to-peer (P2P) system. The file sharing protocol BitTorrent makes use of a DHT to find new peers without using a central tracker. MLDHT, in particular, is an elixir package that provides a mainline DHT implementation according to BEP 05.
     """
   end
 
   defp package do
     [name:        :mldht,
      files:       ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
-     files:       ["apps", "priv", "mix.exs", "README*", "LICENSE*", "license*"],
      maintainers: ["Florian Adamsky"],
      licenses:    ["MIT"],
      links:       %{"GitHub" => "https://github.com/cit/MLDHT"}]
