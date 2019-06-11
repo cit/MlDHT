77d43ef8bb750bead4282b5b8dab5c26362624b8
Florian Adamsky <fa-git@haktar.org>
2019-06-11 19:15:45 +0200

    chore: increase version number to 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MlDHT.Mixfile do
 
   def project do
     [app: :mldht,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded:  Mix.env == :prod,
      start_permanent: Mix.env == :prod,
